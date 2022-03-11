2e6996753b494f2102a1718c574f0af8d6f56ca1
Ronny Svedman <ronny.svedman@elevmail.stockholm.se>
2022-03-11 10:31:36 +0100

    megr

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,5 +6,9 @@ from calculator import add
 def test_add_function_tommy():
     assert add(1, 2) == 3
 
+
+def test_add_function_ronny():
+    assert add(-1, -1) == -2
+
 def test_add_function_eric_gran():
     assert add(hej, hopp) == 203232
