2cef3e2bcc25772aff2304adb1971232245bd47a
Eric Granström <eric.granstrom@elevmail.stockholm.se>
2022-03-11 13:47:19 +0100

    This is a test EricGran,RikardKoni

--- tests/test_calculator_ericgran.py
+++ tests/test_calculator_ericgran.py
@@ -1,5 +1,14 @@
 import pytest
 from calculator import add
 
+def test_add_name_to_store():
+    assert add(EricGran, RikardKoni) == 3
+
+def test_this_is_a_test123():
+    assert the_text "test123"
+
+
+
+
 def test_add_assignment_2_ericgran():
     assert add(1, 2) == 3
