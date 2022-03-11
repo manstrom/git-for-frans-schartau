0e80944f0b9d376981244ad313843f095fd51ee6
Rikard Koniakowski <rikard.koniakowski@elevmail.stockholm.se>
2022-03-11 10:31:26 +0100

    add Rikards test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,5 +6,8 @@ from calculator import add
 def test_add_function_tommy():
     assert add(1, 2) == 3
 
+def test_add_function_rikard():
+    assert add(2, 2) == 4
+
 def test_add_function_eric_gran():
     assert add(hej, hopp) == 203232
