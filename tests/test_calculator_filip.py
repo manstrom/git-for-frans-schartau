eac8cb885d025a0edb5882ca95a7402cff26196b
Filip Milerud <filip.milerud@elevmail.stockholm.se>
2022-03-11 13:44:44 +0100

    update

--- tests/test_calculator_filip.py
+++ tests/test_calculator_filip.py
@@ -5,6 +5,12 @@ import test_calculator
 def test_add_assigment_2_filip():
     assert add(1, 2) == 3
 
+
+# Filip was here
+
+def test_add_assigment_3_filip():
+    assert add(3, 4) == 7
+
 def test_add_assignment_2_louise():
     """Louise updates same file as Filip. Hope it works!"""
     assert add(20, 20) == 40
