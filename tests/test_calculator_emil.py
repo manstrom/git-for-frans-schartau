966581f18c17decbab1dda9cfca4e3d6397e8a46
Tommy Månström <tommy.manstrom@gmail.com>
2023-03-09 11:34:42 +0100

    Tommy change to Emils file

--- tests/test_calculator_emil.py
+++ tests/test_calculator_emil.py
@@ -5,3 +5,6 @@ from src.calculator import add, multiply
 
 def test_add_function():
     assert multiply(2, 2) == 4
+
+def test_add_function_tommy_updates_same_file():
+    assert multiply(10, 2) == 20
