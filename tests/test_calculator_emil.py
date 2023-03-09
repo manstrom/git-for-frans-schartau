7477406129a01777dccf31e7035d35b35eeb4ad0
Emil Eriksson <erikssonemil6080@gmail.com>
2023-03-09 11:27:58 +0100

    Updated testfile test_calculator_emil.py

--- tests/test_calculator_emil.py
+++ tests/test_calculator_emil.py
@@ -3,5 +3,8 @@ import pytest
 from src.calculator import add, multiply
 
 
-def test_add_function():
+def test_multiplication_function():
     assert multiply(2, 2) == 4
+
+def test_multiplication2_function():
+    assert multiply(2, 5) == 10
