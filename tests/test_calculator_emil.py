3fc5bd121fd50bc4115a39450ee5cf0f010e56ba
Julia Finucane <julia.finucane@gmail.com>
2023-03-09 14:50:53 +0100

    Added changes

--- tests/test_calculator_emil.py
+++ tests/test_calculator_emil.py
@@ -3,11 +3,17 @@ import pytest
 from src.calculator import add, multiply
 
 
-def test_multiplication_function():
+def test_multiply_function():
     assert multiply(2, 2) == 4
+
+
+def test_add_function():
+    assert add(2, 2) == 4
+
 
 def test_multiplication2_function():
     assert multiply(2, 5) == 10
+
 
 def test_add_function_tommy_updates_same_file():
     assert multiply(10, 2) == 20
